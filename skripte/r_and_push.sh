3055a98f4c23714aa5c9eaf10d45f25f93194b3e
Ivan Puzek <ivan.puzek@gmail.com>
2017-06-04 23:45:07 +0200

    sredi skriptu

--- skripte/r_and_push.sh
+++ skripte/r_and_push.sh
@@ -1,5 +1,5 @@
 cd /home/ivan/temp_mora
 
 /usr/bin/Rscript R/temp_mora.R
-git pull && git add -A && git commit -m "prava prava proba" && git push
+git pull && git add -A && git commit -m "ajmo ponedjeljak" && git push
 
