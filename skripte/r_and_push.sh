759c125cb4a9f0cb2650b968c4d75b1690a20a55
Ivan Puzek <ivan.puzek@gmail.com>
2017-06-11 22:31:51 +0200

    here we go again

--- skripte/r_and_push.sh
+++ skripte/r_and_push.sh
@@ -1,10 +1,10 @@
 # ova skripta je za bolja vremena
 # kad skužim kako cron koristi ssh ključeve
 
-# /usr/bin/Rscript /home/ivan/temp_mora/R/temp_mora.R
+/usr/bin/Rscript /home/ivan/temp_mora/R/temp_mora.R
 
-# cd /home/ivan/temp_mora
-# HOME=/home/ivan git pull
-# git add -A
-# git commit -m "ajmo utorak"
-# HOME=/home/ivan git push
+cd /home/ivan/temp_mora
+HOME=/home/ivan git pull
+git add -A
+git commit -m "here we go again"
+HOME=/home/ivan git push
